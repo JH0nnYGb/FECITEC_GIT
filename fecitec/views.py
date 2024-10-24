70d7d225d2ee5f6b7709dfa7ad1667943fdac9b9
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-10-24 10:29:52 -0300

    adicionando views

--- fecitec/views.py
+++ fecitec/views.py
@@ -3,6 +3,26 @@ from django.shortcuts import render
 def home_view(request):
     return render(request, 'home.html')
 
+def cronograma_view(request):
+    return render(request, 'cronograma.html')
+
+def submissao_view(request):
+    return render(request, 'submissao.html')
 
 def aprovados_view(request):
     return render(request, 'aprovado.html')
+
+def certificados_view(request):
+    return render(request, 'certificados.html')
+
+def regulamento_view(request):
+    return render(request, 'regulamento.html')
+
+def comissao_view(request):
+    return render(request, 'comissao.html')
+
+def contate_view(request):
+    return render(request, 'contate.html')
+
+def login(request):
+    return render(request, 'login.html')
