02f8632740f4f98987d7913edf29e5f7acb9126b
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-10-31 09:35:15 -0300

    criando configurações iniciais do models

--- fecitec/admin.py
+++ fecitec/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Commission
 
-# Register your models here.
+@admin.register(Commission)
+class ComissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'formation', 'position')
