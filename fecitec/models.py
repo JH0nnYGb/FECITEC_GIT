15e2bd9cd96e6bf5d2d7c2840e8d0039898eafba
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-10-31 09:21:49 -0300

    criando configurações iniciais do models

--- fecitec/models.py
+++ fecitec/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+class Commission(models.Model):
+    name = models.CharField(max_length=100)
+    formation = models.CharField(max_length=100)
+    position = models.CharField(max_length=50)
+    image = StdImageField(upload_to='')
+
+    def __str__(self):
+        return self.name
