7ac8e0578d772f2980c94421c3544afd4305e6f8
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-11-04 09:14:49 -0300

    ajustando models

--- fecitec/models.py
+++ fecitec/models.py
@@ -5,7 +5,7 @@ class Commission(models.Model):
     name = models.CharField(max_length=100)
     formation = models.CharField(max_length=100)
     position = models.CharField(max_length=50)
-    image = StdImageField(upload_to='fecitec/static/images')
+    image = StdImageField(upload_to='photos/', default='photos/default.jpg', blank=True, null=True)
 
     def __str__(self):
         return self.name
