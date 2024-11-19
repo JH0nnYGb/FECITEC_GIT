0549d3f2e21d0f63718a06755954abb5eaf22c85
JH0nnYGb <silvajoneffe@gmail.com>
2024-11-19 18:54:00 -0300

    Ajustes na navbar do admin

--- admin_fecitec/static/Js/navBarReplacement.js
+++ admin_fecitec/static/Js/navBarReplacement.js
@@ -1,3 +1,14 @@
-document.getElementById('profileCheckbox').addEventListener('change', function () {
-    
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const checkbox = document.getElementById('checkbox');
+    const navBar = document.getElementById('navBarStandard');
+
+    checkbox.addEventListener('change', function() {
+      if (checkbox.checked) {
+        console.log('Checkbox marcado');
+        navBar.style.display = 'block';
+      } else {
+        console.log('Checkbox desmarcado');
+        navBar.style.display = 'none';
+      }
+    });
+  });
