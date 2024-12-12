2ebff954b095547d26b894671256642be3960787
JH0nnYGb <silvajoneffe@gmail.com>
2024-12-12 10:39:54 -0300

    Ajustes em CSS/JS e templates para o visual do modal de editar foto de perfil_Participante (Nao finalizado)

---
+++ app_participante/static/Js/modal_profile.js
@@ -0,0 +1,23 @@
+const botaoAbrir = document.getElementById('abrir-modal');
+        const botaoFecharInterno = document.getElementById('fechar-modal-interno');
+        const modalOverlay = document.getElementById('modal-overlay');
+        const botaoFechar = document.querySelector('.fechar-modal');
+
+        function abrirModal() {
+            modalOverlay.style.display = 'flex';
+        }
+
+        function fecharModal() {
+            modalOverlay.style.display = 'none';
+        }
+
+        botaoAbrir.addEventListener('click', abrirModal);
+        botaoFecharInterno.addEventListener('click', fecharModal);
+        botaoFechar.addEventListener('click', fecharModal);
+
+        // Fecha o modal se clicar fora dele
+        modalOverlay.addEventListener('click', function(evento) {
+            if (evento.target === modalOverlay) {
+                fecharModal();
+            }
+        });
